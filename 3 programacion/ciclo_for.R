9d0fc89e5d1eea6428c6506b656bee79c1a0affa
Alfredo Olguin <alfredoo@uber.com>
2017-10-10 15:02:59 -0500

    Ciclos y funciones

---
+++ 3 programacion/ciclo_for.R
@@ -0,0 +1,34 @@
+## Ya conocimos los conceptos básicos de swirl
+## Podemos graficar y hacer forecast automatizados
+
+
+## Suponiendo que queremos hacer una predicción del PIB de muchos países?
+## Cómo usamos los conocimientos de programación para este sentido
+
+## Aprendemos ciclos
+
+## Un ciclo es la repetición simple de una acción
+## cada auto rojo, cuéntalo
+## por cada valor, suma 5 dólares
+## por cada país, realiza un modelo arima!
+
+## Ejercicio 1
+for(i in 1:9){
+  print(i)
+}
+
+## Ejercicio 2
+for(i in 1:9){
+  print(i+5)
+}
+
+## Ejercicio 3: Por cada i, saca la raíz cuadrada
+for(i in 1:9){
+ ?????????????????
+}
+
+
+## Haciendo el arima
+
+
+
