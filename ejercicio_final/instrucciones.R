6a503fdd7ae945d09ae22757e2562eec05950026
Alfredo Olguin <alfredoo@uber.com>
2017-10-12 19:45:50 -0500

    Ejercicio final

---
+++ ejercicio_final/instrucciones.R
@@ -0,0 +1,15 @@
+## Ejercicio final
+
+## El ejercicio consiste en hacer una gráfica "plot(x,y)" por cada uno de los aviones que
+## se encuentran en el archivo de "flights" del paquete "nycflights13"
+## flights contiene cerca de 4,000 aviones distintos que deben ser graficados
+
+## Instrucciones para cargar el archivo
+
+install.packages("nycflights")
+
+library(nycflights13)
+
+data(flights)
+
+View(flights)
