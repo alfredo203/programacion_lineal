3365a276dc1ba02c793ab8714a16ac83bbe56c65
Alfredo Olguin <alfredoo@uber.com>
2017-11-14 19:48:38 -0600

    tips con ejercicio

--- ejercicio_final/instrucciones.R
+++ ejercicio_final/instrucciones.R
@@ -6,7 +6,7 @@
 
 ## Instrucciones para cargar el archivo
 
-install.packages("nycflights")
+install.packages("nycflights13")
 
 library(nycflights13)
 
@@ -22,4 +22,16 @@ names(flights)
 ## tailnum = identificador del avión
 
 ## Recuerda! todo está en swirl (lección 1 en el repositorio)
-## si tienes dificultades para resolver
+## si tienes dificultades para resolver el 
+
+## 1. Extraer solo las columnas interesantes con dplyr (select): time_hour, distance, tailnum
+
+## 2. Jugar con 
+
+install.packages("reshape2") 
+library(reshape2)
+## La función que queremos se llama es dcast(), recuerda que debes definir que time_hour es renglón
+## tailnum es la columna y distance es el valor que va en la casilla.
+
+## 3. Tomar el ciclo for que hicimos en la clase de PIB y cambiar los rangos-nombres de las matrices
+## o dataframes
